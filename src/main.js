8552dde13ad4e43fccb82588001e8f56cafe5130
juaneugenio <leuviah77@gmail.com>
2024-11-30 16:19:09 +0100

    added more post in center container. Added the dropdown menu in the user profile icon.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 /** @format */
+document.addEventListener("DOMContentLoaded", function () {
+	const settingsMenu = document.querySelector(".settings-menu");
+
+	function settingsMenuToggle() {
+		settingsMenu.classList.toggle("settings-menu-height");
+	}
+
+	const userIcon = document.querySelector(".nav-user-icon");
+	userIcon.addEventListener("click", settingsMenuToggle);
+
+	const darkBtn = document.querySelector("#dark-btn");
+	darkBtn.onclick = function () {
+		darkBtn.classList.toggle("dark-btn-on");
+	};
+});
